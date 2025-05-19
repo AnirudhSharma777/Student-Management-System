6be6453fd2b4a4cd3c00bbae2919001b2642f979
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-05-19 06:42:56 +0530

    errorresponse

--- src/main/java/com/management/responseDto/ErrorResponse.java
+++ src/main/java/com/management/responseDto/ErrorResponse.java
@@ -1,12 +1,10 @@
 package com.management.responseDto;
 
-import lombok.Builder;
+
 
 public record ErrorResponse(
     String errorCode,
     String errorMessage
 ) {
 
-    @Builder
-    public ErrorResponse {}
 }
