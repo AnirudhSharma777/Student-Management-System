a821fad767ecc2793eee1ae84f01b6b0e6f93807
Anirudh <97828169+AnirudhSharma777@users.noreply.github.com>
2025-05-19 06:43:26 +0530

    loginresponseDto

--- src/main/java/com/management/responseDto/LoginResponseDto.java
+++ src/main/java/com/management/responseDto/LoginResponseDto.java
@@ -1,12 +1,10 @@
 package com.management.responseDto;
 
-import lombok.Builder;
+
 
 public record LoginResponseDto(
     String token,
     long expiresIn
 ) {
 
-    @Builder
-    public LoginResponseDto {}
 }
